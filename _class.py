77002c4b3365f66df54c345d2d5b1087b831e73c
csdru7 <csdrupad0@gmail.com>
2022-05-17 08:50:30 +0530

    PYTHON

--- _class.py
+++ _class.py
@@ -1,1 +1,11 @@
-print("hello world")
+import pyautogui as py
+import time
+
+print("wait for 5 secs")
+time.sleep(5)
+
+
+for i in range(10):
+    py.write("hello")
+    py.press("enter")
+    time.sleep(1)
